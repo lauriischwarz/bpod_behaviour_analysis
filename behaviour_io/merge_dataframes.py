36834c4729f7f475f53b6c1781ed83275e19440e
lauriischwarz <laura.schwarz@stud.unibas.ch>
2019-12-03 21:49:11 +0000

    rewrites merge and deletes old function, currently untested

--- behaviour_io/merge_dataframes.py
+++ behaviour_io/merge_dataframes.py
@@ -1,35 +1,16 @@
-import os
-import sys
-sys.path.append("../") # go to parent
+
 import pandas as pd
-import warnings
-
-warnings.filterwarnings('ignore')
+import pathlib
 
 
-def merge_DF(AnimalID_list):
-    # output a list of animals ready to pull
-    # Task
-    BpodProtocol = '/Two_Alternative_Choice/'
-    # Behavioural data
-    general_directory = '/Users/lauraschwarz/Documents/Bpod_raw/'
+def merge(dfs):
+    df_all = pd.DataFrame()
+    for df in dfs:
+        df_all = df.append(df)
+    return df_all
 
-    for AnimalID in os.listdir(general_directory):
-        df_file_path = general_directory + AnimalID + BpodProtocol + 'Data_Analysis/' + AnimalID + '_dataframe.csv'
-        if os.path.exists(df_file_path):
-            print('Found data for ' + AnimalID)
 
-    data_frames = []
-    # Read the dataframes and merge them
-    for AnimalID in AnimalID_list:
-        df_file_path = f"{general_directory}{AnimalID}{BpodProtocol}Data_Analysis/{AnimalID}_dataframe.csv"
-        animal_df = pd.read_pickle(df_file_path)
-        data_frames.append(animal_df)
-    animals_df = pd.concat(data_frames, ignore_index=True)
-
-    # Create out directory if it does not exist
-    output_dir = general_directory + '-'.join(AnimalID_list) + '_Analysis/'
-    if not os.path.isdir(output_dir):
-        os.mkdir(output_dir)
-
-    return animals_df
+root_folder = pathlib.Path('/Users/lauraschwarz/Documents/Bpod_raw/')
+fpaths = root_folder.rglob('*_dataframe.csv')
+dfs= [pd.read_csv(str(fpath)) for fpath in fpaths]
+all_df = merge(dfs)
