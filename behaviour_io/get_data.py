8f5157abdfa01469e85ca6548c7f265b102893f3
lauriischwarz <laura.schwarz@stud.unibas.ch>
2019-12-06 13:48:48 +0100

    moves get data funcs to get data

--- behaviour_io/get_data.py
+++ behaviour_io/get_data.py
@@ -1,57 +1,31 @@
-#%load_ext autoreload
-#%autoreload 2
 import os
-import sys
 
-from behaviour_io.constants import ROOT_FOLDER, BPOD_PROTOCOL
-from behaviour_io.read_data import ReadAnimalData
+import load_nested_structs as load_ns
+from behaviour_io.constants import ROOT_FOLDER, BPOD_PROTOCOL, MIN_N_TRIALS
+from behaviour_io.read_data import read_mouse_data_from_bpod
 import custom_functions
-sys.path.append("../")  # go to parent
-import ntpath
 import pandas as pd
-import warnings
-
-warnings.filterwarnings('ignore')
-
-# Select animal to analyze
 
 
-def extract_mouse(animal_id=''):
+def extract_mouse(mouse_id=''):
     output_dir = get_output_directory()
 
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
-    experiment_files, experiment_data, ntrials_distribution, protocols, stimulations, muscimol = \
-        ReadAnimalData(ROOT_FOLDER, animal_id, BPOD_PROTOCOL, printout=True)
-    ExperimentTimes = custom_functions.ParseForTimes(experiment_files)
-    ExperimentDates = custom_functions.ParseForDates(experiment_files)
-    ExperimentDatesPretty = custom_functions.MakeDatesPretty(ExperimentTimes)
+    data = read_mouse_data_from_bpod(mouse_id, BPOD_PROTOCOL, printout=True)
+    experiment_times = custom_functions.ParseForTimes(data['experiment_files'])
+    experiment_dates_pretty = custom_functions.MakeDatesPretty(experiment_times)
 
+    remove_unwanted_values_from_data(data)
+    dfs = [custom_functions.session_to_df(mouse_id,
+                                          experiment_dates_pretty[i],
+                                          exp['SessionData']) for i, exp in enumerate(data['experiment_data'])]
 
-    # Clean data
-    # Remove those experiments fow which a proper time has not been found (old ones that are missing a lot of variables)
-    # Or those with low number of trials
-    minNoOfTr = 30
-    idxToRemove = custom_functions.identifyIdx(ExperimentTimes, ntrials_distribution, minNoOfTr)
-    for idx in idxToRemove:
-        print('deleting data for {} with {} trials'.format(ntpath.basename(experiment_files[idx]),
-                                                           ntrials_distribution[idx]))
-        experiment_data.pop(idx)
-        ExperimentDates.pop(idx)
-        ExperimentDatesPretty.pop(idx)
-        experiment_files.pop(idx)
-        ExperimentTimes.pop(idx)
-        ntrials_distribution.pop(idx)
-        protocols.pop(idx)
-        stimulations.pop(idx)
-        muscimol.pop(idx)
-    # get all data into a dataframe
-    DataFrames = [custom_functions.SessionDataToDataFrame(animal_id, ExperimentDatesPretty[i], exp['SessionData'])
-                  for i, exp in enumerate(experiment_data)]
-    AnimalDF = pd.concat(DataFrames, ignore_index=True)
-    # save the dataframe
-    AnimalDF.to_csv(output_dir + animal_id + '_dataframe.csv')
-    print(AnimalDF.keys())
+    mouse_df = pd.concat(dfs, ignore_index=True)
+
+    save_path = f"{output_dir}{mouse_id}_dataframe.csv"
+    mouse_df.to_csv(save_path)
+    print(mouse_df.keys())
 
 
 def get_output_directory():
@@ -61,3 +35,48 @@ def get_output_directory():
 mouse_ids = ["D1cre06", "D1cre01", "DRD101", "D1cre02", "D1cre03", "D1cre05", "D1cre04", "DRD103", "DRD102"]
 for m_id in mouse_ids:
     extract_mouse(m_id)
+
+
+def get_settings(trial_settings, key):
+    if key in trial_settings[0]["GUIMeta"]:
+        return trial_settings[0]["GUIMeta"][key]["String"][trial_settings[0]["GUI"][key] - 1]
+    return "unknown"
+
+
+def convert_raw_events_matlab_structs_to_dictionaries(trial_raw_events):
+    for i, trial in enumerate(trial_raw_events):
+        trial_raw_events[i] = load_ns._todict(trial)
+    return trial_raw_events
+
+
+def remove_unwanted_values_from_data(data):
+    idx_to_remove = custom_functions.get_unwanted_idx(data['experiment_times'],
+                                                      data['n_trials_distribution'],
+                                                      MIN_N_TRIALS)
+    for d in data:
+        for idx in idx_to_remove:
+            print(f'deleting data for {ntpath.basename(ExperimentFiles[idx])} with {ntrialsDistribution[idx]} trials')
+            d.pop(idx)
+
+
+def Data_to_Dataframe(AnimalID, ExperimentDatesPretty, ExperimentData):
+    # get all data into a dataframe
+    DataFrames = [custom_functions.session_to_df(AnimalID, ExperimentDatesPretty[i], exp['SessionData'])
+                  for i, exp in enumerate(ExperimentData)]
+
+    AnimalDF = pd.concat(DataFrames, ignore_index=True)
+
+    print(AnimalDF.keys())
+
+
+def session_to_remove(AnimalDF):
+    # Select indexes to remove and remove them
+    removesession = []
+
+    IDsToRemove = []  # do not write here
+    for counter, session in enumerate(pd.unique(AnimalDF['SessionTime'])):
+        if counter in removesession:
+            print(session + ' Removed')
+            IDsToRemove.append(session)
+
+    AnimalDF = AnimalDF.loc[~AnimalDF['SessionTime'].isin(IDsToRemove)]
