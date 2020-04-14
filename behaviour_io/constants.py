d5d58f527ea526f6727e9da1991cb9d2b01734ba
lauriischwarz <laura.schwarz@stud.unibas.ch>
2020-04-14 10:14:29 +0100

    modifies mouse ids and paths to data

--- behaviour_io/constants.py
+++ behaviour_io/constants.py
@@ -1,4 +1,69 @@
-ROOT_FOLDER = '/Users/lauraschwarz/Documents/Bpod_raw/'
-BPOD_PROTOCOL = '/Two_Alternative_Choice/'
+ROOT_FOLDER = '/Volumes/Mouseblaster/SJLab/Behaviour/bpod_raw_data/'
+BPOD_PROTOCOL = 'Two_Alternative_Choice'
 MIN_N_TRIALS = 30
+
+
+EXP_CONDITION_DICTIONARY = {'D1cre01': 'D1caspase',
+                         'D1cre02': 'D1caspase',
+                         'D1cre03': 'D1caspase',
+                         'D1cre04': 'D1caspase',
+                         'D1cre05': 'D1caspase',
+                         'D1cre06': 'D1caspase',
+                         'DRD101': 'control',
+                         'DRD102': 'control',
+                         'DRD103': 'control',
+                         'SomFlp04': 'control',
+                         'SomFlp06': 'control'}
+
+MOUSE_ID_DICT = {'D1cre01': 'D1caspase',
+                         'D1cre02': 'D1caspase',
+                         'D1cre03': 'D1caspase',
+                         'D1cre04': 'D1caspase',
+                         'D1cre05': 'D1caspase',
+                         'D1cre06': 'D1caspase',
+                         'DRD101': 'control',
+                         'DRD102': 'control',
+                         'DRD103': 'control',
+                         'SomFlp04': 'control',
+                         'SomFlp06': 'control'}
+
+#EXP_CONDITION_DICTIONARY = {'PV01': 'OHDA',
+                           # 'PV03': 'OHDA',
+                           # 'PV04': 'OHDA',
+                           # 'PV02': 'OHDA',
+                           # 'PV05': 'OHDA',
+                           # 'PV06': 'OHDA',
+                           # 'PV08': 'OHDA',
+                           # 'SomFlp06': 'control',
+                           # 'SomFlp04': 'control',
+                           # 'SomFlp07': 'OHDA',
+                           # 'SomFlp08': 'OHDA',
+                           # 'SomFlp09': 'OHDA',
+                           # 'DRD101': 'control',
+                           # 'DRD102': 'control',
+                           # 'DRD103': 'control',
+
+
+
+#}
+
+#MOUSE_ID_DICT =            {'PV01': 'OHDA',
+                           # 'PV03': 'OHDA',
+                           # 'PV04': 'OHDA',
+                           # 'PV02': 'OHDA',
+                           # 'PV05': 'OHDA',
+                           # 'PV06': 'OHDA',
+                           # 'PV08': 'OHDA',
+                           # 'SomFlp06': 'control',
+                           # 'SomFlp04': 'control',
+                           # 'SomFlp07': 'OHDA',
+                           # 'SomFlp08': 'OHDA',
+                           # 'SomFlp09': 'OHDA',
+                           # 'DRD101': 'control',
+                           # 'DRD102': 'control',
+                           # 'DRD103': 'control',
+#
+#
+#
+#}
 
